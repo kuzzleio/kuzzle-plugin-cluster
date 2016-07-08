3a3538e0a52e6b18212d67876436ecfe20b37abc
Benoît Vidis <bvidis@kaliop.com>
2016-07-08 12:17:31 +0200

    kuz-579

--- run-more-dev.sh
+++ run-more-dev.sh
@@ -39,18 +39,11 @@ fi
 (
     . ./my.env
 
-    # lb
-    export LB_IMAGE=${LB_IMAGE:-kuzzleio/proxy}
-    export LB_VOLUMES="[]"
-    if [ "$LB_PATH" != "" ]; then
-        export LB_VOLUMES="- \"$(readlink -f ${LB_PATH}):/var/app\""
-    fi
-
     # kuzzle
     export KUZ_IMAGE=${KUZ_IMAGE:-kuzzleio/dev:alpine}
-    export KUZ_VOLUMES=""
+    export KUZ_VOLUME=""
     if [ "$KUZ_PATH" != "" ]; then
-        export KUZ_VOLUMES="- \"$(readlink -f ${KUZ_PATH}):/var/app\""
+        export KUZ_VOLUME="- \"$(readlink -f ${KUZ_PATH}):/var/app\""
     fi
 
     envsubst < more.yml.tpl > more.yml
