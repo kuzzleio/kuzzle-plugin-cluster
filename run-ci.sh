2d990ca1d7b4bff6b4ceeb5cfc26eea7439e4ebc
David Bengsch <dbengsch@kaliop.com>
2016-09-06 17:31:07 +0200

    CI run script and websocket port open

--- run-ci.sh
+++ run-ci.sh
@@ -4,4 +4,4 @@ set -e
 
 docker-compose -f "docker-compose/docker-compose-ci.yml" up -d
 sleep 120
-docker exec -ti dockercompose_kuzzle1_1 /bin/sh -c '/scripts/run-test.sh'
+docker exec -ti kuzzle1 /bin/sh -c '/scripts/run-test.sh'
