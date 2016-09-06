9f5b82432c415ddc57a50c9a89ca889c2cc91e3e
David Bengsch <dbengsch@kaliop.com>
2016-09-06 14:05:35 +0200

    CI run script and websocket port open

--- run-ci.sh
+++ run-ci.sh
@@ -2,13 +2,4 @@
 
 set -e
 
-COMPOSE_FILE=docker-compose.yml
-
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-cd "$DIR/docker-compose"
-
-. ./my.env
-
-envsubst < docker-compose.yml.tpl > docker-compose.yml
-
-docker-compose -f "$COMPOSE_FILE" run kuzzle
+docker-compose -f "docker-compose/docker-compose-ci.yml" run kuzzle
