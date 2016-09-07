1337d6e51c5818123a39bc1e952b7523ed6a16d1
David Bengsch <dbengsch@kaliop.com>
2016-09-07 14:51:10 +0200

    Non blocking run script

--- run-ci.sh
+++ run-ci.sh
@@ -6,14 +6,14 @@ docker-compose -f "docker-compose/docker-compose-ci.yml" up -d
 
 sleep 60
 
-docker exec -ti kuzzle1 chmod u+x /scripts/run-kuzzle.sh
-docker exec -ti kuzzle1 chmod u+x /scripts/run-test.sh
-docker exec -ti kuzzle2 chmod u+x /scripts/run-kuzzle.sh
-docker exec -ti kuzzle3 chmod u+x /scripts/run-kuzzle.sh
+docker exec kuzzle1 chmod u+x /scripts/run-kuzzle.sh
+docker exec kuzzle1 chmod u+x /scripts/run-test.sh
+docker exec kuzzle2 chmod u+x /scripts/run-kuzzle.sh
+docker exec kuzzle3 chmod u+x /scripts/run-kuzzle.sh
 
-docker exec -ti kuzzle1 chmod u+x /bin/sh -c '/scripts/run-kuzzle.sh' &
-docker exec -ti kuzzle2 chmod u+x /bin/sh -c '/scripts/run-kuzzle.sh' &
-docker exec -ti kuzzle3 chmod u+x /bin/sh -c '/scripts/run-kuzzle.sh' &
+docker exec -t kuzzle1 /bin/sh -c '/scripts/run-kuzzle.sh' &
+docker exec -t kuzzle2 /bin/sh -c '/scripts/run-kuzzle.sh' &
+docker exec -t kuzzle3 /bin/sh -c '/scripts/run-kuzzle.sh' &
 
 sleep 120
 
