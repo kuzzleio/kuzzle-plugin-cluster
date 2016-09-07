e38c8ec07423d7640449f9618d9ee0178ff30b10
David Bengsch <dbengsch@kaliop.com>
2016-09-07 15:58:55 +0200

    Back to a proper state

--- docker-compose/scripts/debug.sh
+++ docker-compose/scripts/debug.sh
@@ -17,4 +17,13 @@ do
 done
 echo "$(date) - connected successfully to ElasticSearch"
 
-/bin/ash /scripts/run-kuzzle.sh
+echo "Starting Kuzzle..."
+
+npm install
+
+node bin/kuzzle install && pm2 start /config/pm2-dev.json
+
+nohup node-inspector --web-port=8080 --debug-port=7000 > /dev/null 2>&1&
+pm2 sendSignal -s SIGUSR1 KuzzleServer
+
+pm2 logs
--- run-ci.sh
+++ run-ci.sh
@@ -4,17 +4,7 @@ set -e
 
 docker-compose -f "docker-compose/docker-compose-ci.yml" up -d
 
-sleep 60
-
-docker exec kuzzle1 chmod u+x /scripts/run-kuzzle.sh
-docker exec kuzzle1 chmod u+x /scripts/run-test.sh
-docker exec kuzzle2 chmod u+x /scripts/run-kuzzle.sh
-docker exec kuzzle3 chmod u+x /scripts/run-kuzzle.sh
-
-docker exec -t kuzzle1 /bin/sh -c '/scripts/run-kuzzle.sh' > /dev/null &
-docker exec -t kuzzle2 /bin/sh -c '/scripts/run-kuzzle.sh' > /dev/null &
-docker exec -t kuzzle3 /bin/sh -c '/scripts/run-kuzzle.sh' > /dev/null &
-
 sleep 120
 
+docker exec kuzzle1 chmod u+x /scripts/run-test.sh
 docker exec -ti kuzzle1 /bin/sh -c '/scripts/run-test.sh'
