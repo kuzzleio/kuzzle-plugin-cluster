ffc5957b69defa804e2c3354b4977a77e5503aca
Jeff <jeff@kuzzle.io>
2016-09-26 12:07:54 +0200

    another atempt to debug ci

--- run-ci.sh
+++ run-ci.sh
@@ -6,7 +6,7 @@ docker-compose -f "docker-compose/docker-compose-ci.yml" up -d
 
 echo "Sleeping 120"
 sleep 120
-cat /tmp/logs.log
+
 docker exec kuzzle1 chmod u+x /scripts/run-test.sh
 docker exec -ti kuzzle1 /bin/sh -c '/scripts/run-test.sh'
 docker-compose -f "docker-compose/docker-compose-ci.yml" logs loadbalancer kuzzle1 kuzzle2 kuzzle3
