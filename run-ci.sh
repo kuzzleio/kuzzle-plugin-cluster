88e9b1cbb5fb194e5f3cdbf9a8149c7db90851bb
Jeff <jeff@kuzzle.io>
2016-11-03 15:57:06 +0100

    ci

--- run-ci.sh
+++ run-ci.sh
@@ -4,6 +4,7 @@ set -eu
 
 ES=0
 
+docker-compose -f "docker-compose/docker-compose-ci.yml" stop
 docker-compose -f "docker-compose/docker-compose-ci.yml" rm -f
 docker-compose -f "docker-compose/docker-compose-ci.yml" pull
 docker-compose -f "docker-compose/docker-compose-ci.yml" up -d
