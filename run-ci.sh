1c7c36ccc7c73502754cd0a530a7ed7bdd57fcf2
Jeff <jeff@kuzzle.io>
2016-11-03 16:15:32 +0100

    ci

--- run-ci.sh
+++ run-ci.sh
@@ -4,10 +4,9 @@ set -eu
 
 ES=0
 
-docker-compose -f "docker-compose/docker-compose-ci.yml" stop
 docker-compose -f "docker-compose/docker-compose-ci.yml" rm -f
 docker-compose -f "docker-compose/docker-compose-ci.yml" pull
-docker-compose -f "docker-compose/docker-compose-ci.yml" up -d
+docker-compose -f "docker-compose/docker-compose-ci.yml" up -d --verbose
 
 echo "Sleeping 90"
 sleep 90
