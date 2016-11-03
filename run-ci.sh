033e03288b988fc6bbbcc1c78517c760d69795c3
Jeff <jeff@kuzzle.io>
2016-11-03 18:05:42 +0100

    level up sleep to 120

--- run-ci.sh
+++ run-ci.sh
@@ -8,8 +8,8 @@ docker-compose -f "docker-compose/docker-compose-ci.yml" rm -f
 docker-compose -f "docker-compose/docker-compose-ci.yml" pull
 docker-compose -f "docker-compose/docker-compose-ci.yml" up -d
 
-echo "Sleeping 90"
-sleep 90
+echo "Sleeping 120"
+sleep 120
 
 docker exec kuzzle1 chmod u+x /scripts/run-test.sh
 
