dcf040cc14cd58ec2fda3246618884e59230284b
Jeff <jeff@kuzzle.io>
2016-11-03 17:51:30 +0100

    typo

--- run-ci.sh
+++ run-ci.sh
@@ -14,7 +14,7 @@ sleep 90
 docker exec kuzzle1 chmod u+x /scripts/run-test.sh
 
 if ! (docker exec -ti kuzzle1 /bin/sh -c '/scripts/run-test.sh'); then
-    docker-compose -f "docker-compose/docker-compose-ci.yml" logs dockercompose_elasticsearch_1 loadbalancer kuzzle1 kuzzle2 kuzzle3
+    docker-compose -f "docker-compose/docker-compose-ci.yml" logs elasticsearch loadbalancer kuzzle1 kuzzle2 kuzzle3
     ES=1
 fi
 
